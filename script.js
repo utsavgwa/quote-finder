42afb5d0b93b14af44c9bc1640844d3f4078d6d2
Utsav Singh <utsavgwa@gmail.com>
2023-01-12 02:30:42 +0530

    added loader animation

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const tText = document.getElementById("quote");
 const tAuthor = document.getElementById("author");
 const newQuoteBtn = document.getElementById("new-quote");
 const twitterBtn = document.getElementById("twitter");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// loading function to show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading function
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // function to show new quote
 function newQuote() {
+  // here sinceon pressing the button weare bypassing the getQuote function
+  loading();
+
   // to pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,12 +40,17 @@ function newQuote() {
   } else {
     tText.classList.remove("long-quote");
   }
+  // set Quote, hide loader so calling complete() function
+  complete();
   tText.textContent = quote.text;
 }
 
 // get quote from API
 async function getQuote() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
+  //right on this line, i can also attach a proxyUrl for solving cors problem
+
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
@@ -38,6 +59,7 @@ async function getQuote() {
     // getQuote
   }
 }
+
 // to tweet a quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=I recently read an encouraging quote, so here it is: "${tText.textContent}" - ${tAuthor.textContent}.`;
